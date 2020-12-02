8020bfb6857fcd1c15d0bf2bbf918a38d0d39697
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-12-02 08:18:31 -0300

    Add test plan to limit execution time

--- HackerRankSwift/Tests/HackerRankSwiftTests/ArrayTests.swift
+++ HackerRankSwift/Tests/HackerRankSwiftTests/ArrayTests.swift
@@ -2,6 +2,13 @@ import XCTest
 @testable import HackerRankSwift
 
 final class ArrayTests: XCTestCase {
+
+    override func setUp() {
+        // 60 is the minimum...
+        executionTimeAllowance = 60
+        continueAfterFailure = true
+    }
+
     func testLeftRotation() {
         let expected = [5, 1, 2, 3, 4]
         let actual = rotLeft(a: [1, 2, 3, 4, 5], d: 4)
@@ -12,13 +19,14 @@ final class ArrayTests: XCTestCase {
         let expected = 3
         let actual = minimumSwaps(arr: [1, 3, 5, 2, 4, 6, 7])
         XCTAssertEqual(expected, actual)
-
-        // [1, 3, 5, 2, 4, 6, 7]
-        // [1, 2, 5, 3, 4, 6, 7] - 1
-        // [1, 2, 5, 3, 4, 6, 7] - 2
     }
 
-//    static var allTests = [
-//        ("testLeftRotation", testLeftRotation),
-//    ]
+    func testMinimumSwapsHugeInput() {
+        let expected = 50000
+
+        let input14 = Array(1...100000)
+        let actual = minimumSwaps(arr: input14)
+
+        XCTAssertEqual(expected, actual)
+    }
 }
